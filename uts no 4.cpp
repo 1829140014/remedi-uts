37aae6ff6e64434af0834336d85cf528cdf97542
muhammad. nur ichsan <49121655+1829140014@users.noreply.github.com>
2019-04-02 11:31:14 +0800

    Rename uts no 2.cpp to uts no 4.cpp

---
+++ uts no 4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+struct data_mhs
+{
+    string nama;
+    float ips_semester_1;
+    float ipk;
+} a,b;
+
+int main()
+{
+    a. nama = "ajis gagap";
+    a. ips_semester_1 = 3,0;
+    a. ipk = 2,5;
+    b. nama = "tumming"
+    b. ips_semester_1 = 3,1;
+    b. ipk = 3,0;
+    
+    cout<< " nama : :<< a.nama <<endl;
+    cout<< "ips_semester_1 : " << a.ips_semester_1<<endl;
+    cout<< "ipk : " << a.ipk<<endl;
+    cout<< "\n";
+    
+    cout<< " nama : :<< b.nama <<endl;
+    cout<< "ips_semester_1 : " << b.ips_semester_1<<endl;
+    cout<< "b.ipk : " << b.ipk<<endl;
+    
+  return 0;
+  }
+    
